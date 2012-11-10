460caf7893956782310b2687e76fc82c894b7cb4
Nyaran <nyayukko@gmail.com>
2012-11-10 22:11:02 +0100

    Typo fix on previous commit

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -1021,10 +1021,10 @@ class ConfigPostProcessing:
         return _munge(t)
 
     @cherrypy.expose
-    def savePostProcessing(self, naming_pattern=None, naming_multi_ep=None, naming_strip_year=None,
+    def savePostProcessing(self, naming_pattern=None, naming_multi_ep=None,
                     xbmc_data=None, mediabrowser_data=None, synology_data=None, sony_ps3_data=None, wdtv_data=None, tivo_data=None,
                     use_banner=None, keep_processed_dir=None, process_automatically=None, rename_episodes=None,
-                    move_associated_files=None, tv_download_dir=None, naming_custom_abd=None, naming_abd_pattern=None, namimg_strip_year=None):
+                    move_associated_files=None, tv_download_dir=None, naming_custom_abd=None, naming_abd_pattern=None, naming_strip_year=None):
 
         results = []
 
