708e48ab47a87ddef7aa5a6bd85c55cae7454204
Nyaran <nyayukko@gmail.com>
2013-02-23 22:30:58 +0100

    Fix download subtitle notification if subtitle already exists

--- sickbeard/tv.py
+++ sickbeard/tv.py
@@ -1160,6 +1160,8 @@ class TVEpisode(object):
             return
         logger.log(str(self.show.tvdbid) + ": Downloading subtitles for episode " + str(self.season) + "x" + str(self.episode), logger.DEBUG)
         
+        previous_subtitles = self.subtitles
+
         try:
             subtitles = subliminal.download_subtitles([self.location], languages=sickbeard.SUBTITLES_LANGUAGES, services=sickbeard.subtitles.getEnabledServiceList(), force=False, multi=True, cache_dir=sickbeard.CACHE_DIR)
             
@@ -1167,22 +1169,20 @@ class TVEpisode(object):
             logger.log("Error occurred when downloading subtitles: " + str(e), logger.DEBUG)
             return
         
-        if subtitles:
-            subtitleList = []
-            for video in subtitles:
-                for subtitle in subtitles.get(video):
-                    subtitleList.append(subtitle.language.name)
-            
+        self.refreshSubtitles()
+        self.subtitles_searchcount = self.subtitles_searchcount + 1
+        self.subtitles_lastsearch = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.saveToDB()
+        
+        newsubtitles = set(self.subtitles).difference(set(previous_subtitles))
+        
+        if newsubtitles:
+            subtitleList = (subliminal.language.Language(x).name for x in newsubtitles)
             logger.log(str(self.show.tvdbid) + ": Downloaded " + ", ".join(subtitleList) + " subtitles for episode " + str(self.season) + "x" + str(self.episode), logger.DEBUG)
             
             notifiers.notify_subtitle_download(self.prettyName(), ", ".join(subtitleList))
         else:
             logger.log(str(self.show.tvdbid) + ": No subtitles downloaded for episode " + str(self.season) + "x" + str(self.episode), logger.DEBUG)
-        
-        self.refreshSubtitles()
-        self.subtitles_searchcount = self.subtitles_searchcount + 1
-        self.subtitles_lastsearch = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.saveToDB()
         
         return subtitles
 
